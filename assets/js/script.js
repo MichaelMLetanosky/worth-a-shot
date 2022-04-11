c09904ad5d0d13efa9a90a3fd0f2069430d0a749
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-04-11 14:28:17 -0400

    Added functionality for pushing search button and getting value from search for bars

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
 //Global Variables
 
+//Script
 
-//Scriptgit 
+function findBars(event) {
+    event.preventDefault();
+    console.log("clicked location search");
+
+    let cityName = document.getElementById("location").value.trim();
+    console.log(cityName);
+    if (!cityName) {
+        alert("Please enter the name of a city");
+        return;
+    };
+};
+
+document.getElementById("locationSrch").addEventListener("click", findBars);
