41a7d71d83f3f00d129d05def66e6c79c3821411
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-04-11 14:34:29 -0400

    Added functionality for pushing search button and getting value from search for ingredients

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,18 @@ function findBars(event) {
     };
 };
 
+function findDrinks(event) {
+    event.preventDefault();
+    console.log("clicked ingredient search");
+
+    let ingredientName = document.getElementById("ingredient").value.trim();
+    console.log(ingredientName);
+    if (!ingredientName) {
+        alert("Please enter an ingredient");
+        return;
+    };
+};
+
+
 document.getElementById("locationSrch").addEventListener("click", findBars);
+document.getElementById("ingredientSrch").addEventListener("click", findDrinks);
