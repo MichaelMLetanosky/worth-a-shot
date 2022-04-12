22591ff672bf353d9796639a3595021b71a72a56
Mike Sands <mikesands94@gmail.com>
2022-04-12 11:28:16 -0400

    added api to get drinks data, began drilling into array

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,8 +24,27 @@ function findDrinks(event) {
         alert("Please enter an ingredient");
         return;
     };
+
+        // API call to receive drink data by name ie. margaraita, rum, gin etc. 
+        const api_url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${ingredientName}`
+        async function getDrinks() {
+        const response = await fetch(api_url);
+        const data = await response.json();
+        console.log(data);
+        data.drinks[0].strDrink;
+        console.log(data.drinks[0].strDrink);
+        //For Loop to cycle 
+        
+        //create element
+
+        //append to display area
+    }
+    getDrinks ();
+
 };
 
 
 document.getElementById("locationSrch").addEventListener("click", findBars);
 document.getElementById("ingredientSrch").addEventListener("click", findDrinks);
+
+
