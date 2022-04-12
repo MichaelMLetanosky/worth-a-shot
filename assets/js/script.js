64fae134f0295b5d9ac67298cfda5e2183fce5bf
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-04-12 13:26:11 -0400

    Added functionality to search for a location, get coordinates, and search google places

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 //Global Variables
 
+const googleApiKey = "AIzaSyCbc5fH-NekhIhEMdTAUjxzRZHusawIAOA"
+
 //Script
 
 function findBars(event) {
@@ -12,7 +14,42 @@ function findBars(event) {
         alert("Please enter the name of a city");
         return;
     };
+
+    fetch (`https://maps.googleapis.com/maps/api/geocode/json?address=${cityName}&key=${googleApiKey}`)
+        .then(response => {
+            //Check that response came back good
+            if (!response.ok) {
+                alert("Could not find location, please try again");
+                return;
+            };
+
+            return response.json();
+        })
+        .then(data => {
+            console.log(data) 
+            let locationLat = data.results[0].geometry.location.lat
+            let locationLng = data.results[0].geometry.location.lng
+            console.log (locationLat)
+            console.log (locationLng)
+            showBars (locationLat, locationLng)
+        })   
 };
+
+function showBars(x, y) {
+    fetch (`https://floating-headland-95050.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${x}%2c${y}&radius=8500&type=bar&key=${googleApiKey}`)
+        .then(response => {
+            //Check that response came back good
+            if (!response.ok) {
+                alert("Could not find location, please try again");
+                return;
+            };
+
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.results) 
+        })
+}
 
 function findDrinks(event) {
     event.preventDefault();
