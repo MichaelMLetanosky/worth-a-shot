4381759489c8a722836ad4da91df7533b573aee7
Mike Sands <mikesands94@gmail.com>
2022-04-12 14:41:25 -0400

    updated beginning stages of drink display functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,10 +31,35 @@ function findDrinks(event) {
         const response = await fetch(api_url);
         const data = await response.json();
         console.log(data);
+        // Retrieve specific properties from object array
         data.drinks[0].strDrink;
+        data.drinks[0].strDrinkThumb;
+        data.drinks[0].strIngredient1;
+        data.drinks[0].strIngredient2;
+        data.drinks[0].strIngredient3;
+        data.drinks[0].strIngredient4;
         console.log(data.drinks[0].strDrink);
-        //For Loop to cycle 
+        console.log(data.drinks[0].strDrinkThumb);
+        console.log(data.drinks[0].strIngredient1);
+        console.log(data.drinks[0].strIngredient2);
+        console.log(data.drinks[0].strIngredient3);
+        console.log(data.drinks[0].strIngredient4);
+
         
+            function displayDrinks(data) {
+                const drink = data.drinks[0];
+                const drinkDiv = document.querySelector('#drinkDisplay');
+                const drinkName = drink.strDrink;
+                const heading = document.createElement("h1");
+                heading.innerHTML = drinkName;
+                drinkDiv.appendChild(heading);
+
+                const drinkImg = document.createElement("img");
+                drinkImg.src = drink.strDrinkThumb;
+                drinkDiv.appendChild(drinkImg);
+                
+
+            }
         //create element
 
         //append to display area
