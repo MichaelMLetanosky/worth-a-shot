bc20626725f553adcd9494b6a792acaab00220d8
Ricky Thakar <rickythakar@gmail.com>
2022-04-13 14:13:22 -0400

    setup function and event listener for yes button. Next step is to setup no button so that it can redirect user to a link

--- assets/js/age-checker.js
+++ assets/js/age-checker.js
@@ -1,19 +1,38 @@
+console.log("age-check js is hooked up")
 // Get the modal
+//declaring dom elements
 var modal = document.getElementById("modal");
-
-// Get the button that opens the modal
-var yesBtn = document.querySelector("yesBtn");
-var noBtn = document.querySelector("noBtn")
+var yesBtn = document.querySelector(".yesBtn");
+var noBtn = document.querySelector(".noBtn");
 
 // When the user clicks on the button, open the modal
 window.onload = function() {
   document.getElementById("modal").style.display = "block";
-
-}
-// yesBtn.onclick = function(event) {
-//   console.log("Clicked yes");
-// }
+};
 
 
+//function for yes button
+function yesFunction(event){
+  document.getElementById("modal").style.display = "none";
+  console.log("yesFunction");
+};
 
-// noBtn.onclick
+//function for no button
+function noFunction(event){
+  
+  console.log("noFunction");
+};
+//call function for yesbtn
+yesBtn.addEventListener ("click", yesFunction);
+
+noBtn.addEventListener ("click", noFunction);
+
+
+
+
+
+
+
+
+
+
