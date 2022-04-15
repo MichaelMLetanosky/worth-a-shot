3025348715932ff24ee2d444347cf5174c2806ef
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-04-14 23:44:09 -0400

    Finished styling of cards

--- assets/js/favoritescript.js
+++ assets/js/favoritescript.js
@@ -1,3 +1,4 @@
+// Global Variables, including the google API key and the locally stored favs
 const googleApiKey = "AIzaSyCbc5fH-NekhIhEMdTAUjxzRZHusawIAOA";
 let favDrinkData = JSON.parse(localStorage.getItem("myDrinks"));
 let favBarData = JSON.parse(localStorage.getItem("myBars"));
@@ -35,16 +36,20 @@ function buildDrinkCards () {
 
 //Generate Bar Cards
 function buildBarCards () {
+
+    // Displays Title of Section
+    document.querySelector("#favBarTitle").style.display = "block";
     
     for (i=0; i < favBarData.length; i++) {
         // let favBarCard = document.createElement("div")
-        let favBarCard = document.createElement("div")
-        document.querySelector("#favBarCont").appendChild(favBarCard)
+        let favBarCard = document.createElement("div");
+        favBarCard.classList.add("favBars");
+        document.querySelector("#favBarCont").appendChild(favBarCard);
 
         //Set Photo using a google referese number
         let favBarPhoto = favBarData[i].Photo;
-        let favBarPhotoCont = document.createElement("img")
-        favBarPhotoCont.src = `https://maps.googleapis.com/maps/api/place/photo?maxheight=500&maxwidth=500&photo_reference=${favBarPhoto}&key=${googleApiKey}`;
+        let favBarPhotoCont = document.createElement("img");
+        favBarPhotoCont.src = `https://maps.googleapis.com/maps/api/place/photo?maxheight=500&maxwidth=600&photo_reference=${favBarPhoto}&key=${googleApiKey}`;
         favBarCard.appendChild(favBarPhotoCont);
         
         // Set Bar Name
@@ -54,25 +59,20 @@ function buildBarCards () {
         favBarCard.appendChild(favBarNameCont);
 
         // Set Bar Address
-        let favBarAddressCont = document.createElement("p")
+        let favBarAddressCont = document.createElement("p");
         favBarAddressCont.innerHTML = favBarData[i].Address;
         favBarCard.appendChild(favBarAddressCont);
 
         // Set Bar Phone
-        let favBarPhoneCont = document.createElement("p")
-        favBarPhoneCont.innerHTML = favBarData[i].Phone;
+        let favBarPhoneCont = document.createElement("p");
+        favBarPhoneCont.innerHTML = `Phone: ${favBarData[i].Phone}`;
         favBarCard.appendChild(favBarPhoneCont);
 
         // Set Bar Rating
-        let favBarRatingCont = document.createElement("p")
-        favBarRatingCont.innerHTML = favBarData[i].Rating;
+        let favBarRatingCont = document.createElement("p");
+        favBarRatingCont.innerHTML = `Rating: ${favBarData[i].Rating}`;
         favBarCard.appendChild(favBarRatingCont);
-    }
-
-    // let barName = data.results[i].name;
-    // let barNameCont = document.createElement("a");
-    // barNameCont.innerHTML = barName;
-    // document.querySelectorAll(".barCard")[i].appendChild(barNameCont);
+    };
 };
 
 //Initial Call
